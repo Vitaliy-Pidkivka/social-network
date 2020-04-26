4f17b8e025f78022314367ce0e18c47af0e8550b
vpidkivka <vpidkivka@eliteit.org>
2020-04-26 11:07:30 +0300

    add axios library, use users API

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,72 +1,76 @@
 import React from 'react'
 import styles from './Users.module.scss'
 import Button from "../Shared/Button/Button";
-
+import * as axios from 'axios'
+import avatarUrl from '../../assets/images/user-avatar.png'
 const Users = (props) => {
-   if (!props.users.length){
-       props.setUsers([
-           {
-               id: 1,
-               followed: true,
-               avatarUrl: 'https://cdn2.iconfinder.com/data/icons/men-avatars/33/man_2-512.png',
-               fullName: 'Dmutro',
-               status: 'playing valorant...',
-               location: {city: 'Lviv', country: 'Ukraine'},
-           },
-           {
-               id: 2,
-               followed: true,
-               avatarUrl: 'https://cdn2.iconfinder.com/data/icons/men-avatars/33/man_7-512.png',
-               fullName: 'Vasia',
-               status: 'react vorever',
-               location: {city: 'Minsk', country: 'Belarus'},
-           },
-           {
-               id: 3,
-               followed: true,
-               avatarUrl: 'https://cdn1.iconfinder.com/data/icons/avatars-1-5/136/84-512.png',
-               fullName: 'Tania',
-               status: 'cycling the best',
-               location: {city: 'Lviv', country: 'Ukraine'},
-           },
-           {
-               id: 4,
-               followed: false,
-               avatarUrl: 'https://cdn4.iconfinder.com/data/icons/men-avatars-icons-set-1/256/21-512.png',
-               fullName: 'Krzysztof',
-               status: 'playing valorant...',
-               location: {city: 'Krakov', country: 'Poland'},
-           },
-       ])
-   }
+
+    if (!props.users.length) {
+
+        // props.setUsers([
+        //     {
+        //         id: 1,
+        //         followed: true,
+        //         avatarUrl: 'https://cdn2.iconfinder.com/data/icons/men-avatars/33/man_2-512.png',
+        //         fullName: 'Dmutro',
+        //         status: 'playing valorant...',
+        //         location: {city: 'Lviv', country: 'Ukraine'},
+        //     },
+        //     {
+        //         id: 2,
+        //         followed: true,
+        //         avatarUrl: 'https://cdn2.iconfinder.com/data/icons/men-avatars/33/man_7-512.png',
+        //         fullName: 'Vasia',
+        //         status: 'react vorever',
+        //         location: {city: 'Minsk', country: 'Belarus'},
+        //     },
+        //     {
+        //         id: 3,
+        //         followed: true,
+        //         avatarUrl: 'https://cdn1.iconfinder.com/data/icons/avatars-1-5/136/84-512.png',
+        //         fullName: 'Tania',
+        //         status: 'cycling the best',
+        //         location: {city: 'Lviv', country: 'Ukraine'},
+        //     },
+        //     {
+        //         id: 4,
+        //         followed: false,
+        //         avatarUrl: 'https://cdn4.iconfinder.com/data/icons/men-avatars-icons-set-1/256/21-512.png',
+        //         fullName: 'Krzysztof',
+        //         status: 'playing valorant...',
+        //         location: {city: 'Krakov', country: 'Poland'},
+        //     },
+        // ])
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            console.log(response)
+            props.setUsers(response.data.items)
+        })
+
+    }
     return (
         <div className={styles['users']}>
             {props.users.map(user =>
                 <div className={styles['user']}>
                     <div className={styles['user__box']}>
-                        <img src={user.avatarUrl}
+                        <img src={user.photos.small ? user.photos.small : avatarUrl }
                              alt="avatar"
                              className={styles['user__avatar']}/>
                         <Button value={user.followed ? 'Unfollow' : 'Follow'}
-                                onClick={user.followed ? () => {
-                                    props.unfollow(user.id)
-                                } : () => {
-                                    props.follow(user.id)
-                                }}
+                                onClick={user.followed ? () => {props.unfollow(user.id) } : () => {  props.follow(user.id) }}
                                 sizeClass="small"
                                 typeClass="purple"
                                 className={styles['user__btn']}/>
                     </div>
                     <div className={styles['user__box']}>
-                        <h3 className={styles['user__name']}> {user.fullName}</h3>
-                        <p className={styles['user__status']}>{user.status}</p>
+                        <h3 className={styles['user__name']}> {user.name}</h3>
+                        <p className={styles['user__status']}>{user.status ? user.status : 'status will be here'}</p>
                     </div>
                     <div className={styles['user__box']}>
-                        <p className={styles['user__city']}>{user.location.city} </p>
-                        <p className={styles['user__country']}>{user.location.country}</p>
+                        <p className={styles['user__city']}>{"user.location.city"} </p>
+                        <p className={styles['user__country']}>{"user.location.country"}</p>
                     </div>
                 </div>)}
-            <Button value="SHOW MORE" typeClass="aqua" className={styles['users__btn']}/>
+            {props.users.length && <Button value="SHOW MORE" typeClass="aqua" className={styles['users__btn']}/>}
         </div>
     )
 
