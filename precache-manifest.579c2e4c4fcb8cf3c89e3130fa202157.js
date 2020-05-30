2c6e68c97381b9cd974054ebb137ae764d68127d
vpidkivka <vpidkivka@eliteit.org>
2020-05-30 21:57:53 +0300

    Updates

---
+++ precache-manifest.579c2e4c4fcb8cf3c89e3130fa202157.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74a5170a67fb3877c6b788b7488f6ee4",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "caf2978d628281f3536c",
+    "url": "/social-network/static/css/4.f7348605.chunk.css"
+  },
+  {
+    "revision": "68209552ae7dc453165e",
+    "url": "/social-network/static/css/5.b17c1089.chunk.css"
+  },
+  {
+    "revision": "0065ebb0108b928a513e",
+    "url": "/social-network/static/css/6.8a4acd89.chunk.css"
+  },
+  {
+    "revision": "54b0c996f7c5e995e97f",
+    "url": "/social-network/static/css/7.d621ebf4.chunk.css"
+  },
+  {
+    "revision": "fe87e808c398f438f022",
+    "url": "/social-network/static/css/main.651eb5fb.chunk.css"
+  },
+  {
+    "revision": "9fa000f98196331c4c52",
+    "url": "/social-network/static/js/0.f89abc49.chunk.js"
+  },
+  {
+    "revision": "c4c02a0284db3154ad72",
+    "url": "/social-network/static/js/3.f0f4d552.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-network/static/js/3.f0f4d552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caf2978d628281f3536c",
+    "url": "/social-network/static/js/4.bb7305b0.chunk.js"
+  },
+  {
+    "revision": "68209552ae7dc453165e",
+    "url": "/social-network/static/js/5.901868af.chunk.js"
+  },
+  {
+    "revision": "0065ebb0108b928a513e",
+    "url": "/social-network/static/js/6.ba8522ef.chunk.js"
+  },
+  {
+    "revision": "54b0c996f7c5e995e97f",
+    "url": "/social-network/static/js/7.2838ab4e.chunk.js"
+  },
+  {
+    "revision": "fe87e808c398f438f022",
+    "url": "/social-network/static/js/main.33f0207f.chunk.js"
+  },
+  {
+    "revision": "277c7b23da1de5d2af9e",
+    "url": "/social-network/static/js/runtime-main.f519b044.js"
+  },
+  {
+    "revision": "65c343501d9e019069596ad0923aaef0",
+    "url": "/social-network/static/media/HistoriaDemo-Regular.65c34350.woff"
+  },
+  {
+    "revision": "b861d10fb9fb37f50a1c05002deb295a",
+    "url": "/social-network/static/media/HistoriaDemo-Regular.b861d10f.woff2"
+  },
+  {
+    "revision": "9abc40a1a46345718d1b447f7b190f40",
+    "url": "/social-network/static/media/OgoweyDemoRegular.9abc40a1.woff"
+  },
+  {
+    "revision": "c8b0ac91875d01223473b3fe02796d34",
+    "url": "/social-network/static/media/OgoweyDemoRegular.c8b0ac91.woff2"
+  },
+  {
+    "revision": "12a06dd4145e95830990aa62ee2ff2fe",
+    "url": "/social-network/static/media/Smeshariki2007Fixed-Regular.12a06dd4.woff2"
+  },
+  {
+    "revision": "a9cb5f4db33f8e35ffc305aac21d6b3f",
+    "url": "/social-network/static/media/Smeshariki2007Fixed-Regular.a9cb5f4d.woff"
+  },
+  {
+    "revision": "9203aa6dc848b8b068ac303d5a9628dd",
+    "url": "/social-network/static/media/bg-profile.9203aa6d.jpg"
+  },
+  {
+    "revision": "00c3295544e5c7d0588fafeea7812efc",
+    "url": "/social-network/static/media/logo.00c32955.png"
+  },
+  {
+    "revision": "6329c0a65b34d8effdccd8bed9cd749d",
+    "url": "/social-network/static/media/next-arrow.6329c0a6.svg"
+  },
+  {
+    "revision": "6962a1740eec94dc23305555450f1650",
+    "url": "/social-network/static/media/prev-arrow.6962a174.svg"
+  },
+  {
+    "revision": "d60e2ec58f188dafec165fd6048b2d41",
+    "url": "/social-network/static/media/user-avatar.d60e2ec5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.214425319de75e7c31ed763d2ed0671c.js"
+  "/social-network/precache-manifest.579c2e4c4fcb8cf3c89e3130fa202157.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/social-network/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
